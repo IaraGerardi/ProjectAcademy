e61574d9b29c757b56e24d96704ec6c881616953
Cristian Vera <cristian.vera@arbusta.net>
2022-09-06 15:06:32 -0300

    Bloque Mission

---
+++ front/src/pages/Mission/Mission.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Mission.css";
+
+export default function Mission() {
+  return (
+    <div className="content">
+      <div className="content_two">
+        <h1 className="paragraph_misions">Misión</h1>
+        <p className="paragraph_fontsize">
+          Acompañamos a las personas en sus procesos de transformación personal
+          y profesional para que logren construir una vida con sentido
+      </p>
+
+        <p className="paragraph_bold">
+          Quienes viven la experiencia V_Camp aprenden a:
+        </p>
+
+      <ul className="list">
+          <li className="lis">
+            <p className="paragraph">
+              Conocerse, Valorarse y hacerse cargo de su potencial.
+            </p>
+          </li>
+
+          <li className="lis">
+            <p className="paragraph">Construir su proposito.</p>
+          </li>
+
+          <li className="lis">
+            <p className="paragraph">
+              Crear su propia carrera personalprofesional.
+            </p>
+          </li>
+      </ul>
+
+      </div>
+    </div>
+  );
+}
