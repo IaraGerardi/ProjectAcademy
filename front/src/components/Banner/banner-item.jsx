257f869705ebd1610afd2fbef8e6035d9697579d
Arbusta <iara.gerardi@arbusta.net>
2022-09-06 15:40:27 -0300

    banner

--- front/src/components/Banner/banner-item.jsx
+++ front/src/components/Banner/banner-item.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 export function CarrouselItem({ news , index }) {
   return (
     <li className="carrouselItem" key={index.toString()}>
-      <NavLink to={news.redirect}>
         <div className="contenido">
           <h2>{news.title}</h2>
           <p>{news.description}</p>
         </div>
         <img src={news.bgImage} alt={news.title}/>
-      </NavLink>
     </li>
   )
 }
