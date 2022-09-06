c98d4257c035cfbd85e6ad915f3caf975c8b16c8
Macarena-Leiva <macarena.leiva@arbusta.net>
2022-09-06 16:06:56 -0300

    assets

--- front/src/pages/Home/components/header/header.js
+++ front/src/pages/Home/components/header/header.js
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import logo from '../../assets/vnegro 1.png'
 export const header = () => {
   return (
-    <div>header</div>
+    <nav>
+        <ul>
+          <li><img src={logo} /></li>
+          <li><button>Ingresa a tu portal</button></li>
+        </ul>
+    </nav>
   )
 }
