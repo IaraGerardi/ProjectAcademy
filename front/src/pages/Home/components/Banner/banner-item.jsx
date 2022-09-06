111ed02850655e239d6a1af309624e057eb0079a
Arbusta <iara.gerardi@arbusta.net>
2022-09-06 17:16:26 -0300

    banner0 git commit -m banner git commit -m banner0

--- front/src/pages/Home/components/Banner/banner-item.jsx
+++ front/src/pages/Home/components/Banner/banner-item.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export function CarrouselItem({ news , index }) {
+export function BannerItem({ news , index }) {
   return (
     <li className="carrouselItem" key={index.toString()}>
-      <NavLink to={news.redirect}>
-        <div className="contenido">
+        <div className="contentItemBanner">
           <h2>{news.title}</h2>
           <p>{news.description}</p>
         </div>
         <img src={news.bgImage} alt={news.title}/>
-      </NavLink>
     </li>
   )
 }
--- front/src/pages/Home/components/Banner/banner.jsx
+++ front/src/pages/Home/components/Banner/banner.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 import news from "./news.json";
-import { CarrouselItem } from "./banner-item";
-
-export function Carrousel() {
+import { BannerItem } from "./banner-item";
+import "./banner.css"
+export function Banner() {
     const [item, setItem] = useState(0);
-    const bannerItems = news.map((news, index) => <CarrouselItem news={news} index={index} />);
+
+    const bannerItems = news.map((news, index) => <BannerItem news={news} index={index} />);
+
     const itemLess = () => { item < 1 ? setItem(news.length - 1) : setItem(item - 1) }
     const itemPlus = () => { item >= news.length - 1 ? setItem(0) : setItem(item + 1) }
     // let a = setInterval(sumarItem, 2000);
 
     return (
-        <div className="carrousel">
-            <div className="botonesCarrousel">
-                <div onClick={itemLess} className="flecha Izq">
-                    <span className="iconify botonSuma" data-icon="akar-icons:arrow-left" />
+        <div className="banner">
+            <div className="buttonsBanner">
+                <div onClick={itemLess} className="arrow left">
+                    <span className="iconify buttonLeft" data-icon="akar-icons:arrow-left" />
                 </div>
-                <div onClick={itemPlus} className="flecha Der">
-                    <span className="iconify botonSuma" data-icon="akar-icons:arrow-right" />
+                <div onClick={itemPlus} className="arrow right">
+                    <span className="iconify buttonRight" data-icon="akar-icons:arrow-right" />
                 </div>
             </div>
-            <ul className="novedades">{bannerItems[item]}</ul>
+            <ul className="bannerUL">{bannerItems[item]}</ul>
         </div>
     )
 }
