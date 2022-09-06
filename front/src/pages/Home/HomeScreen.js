d4b5d584037aa739f6cfc9e64cc15f811af01444
SebastianAvila00 <sebastian.avila@arbusta.net>
2022-09-06 17:13:46 -0300

    experiencia text

--- front/src/pages/Home/HomeScreen.js
+++ front/src/pages/Home/HomeScreen.js
@@ -2,17 +2,26 @@
 import Footer from "./components/Footer/Footer";
 import Mission from "./components/Mission/Mission"
 import Proposal from "./components/Proposal/Propuesta"
-import {Header} from './components/header/Header'
 
 function HomeScreen() {
   return (
     <div className="HomeScreen">
-      <Header />
-      <Proposal/>
-      <Mission/>
-      <Footer/>
+      <Proposal />
+      <Mission />
+
+      <div className="cont-experience">
+          <p className="text-transform">Viví tu transformación personal.</p>
+          <p className="text-experience">Viví tu experiencia V_Camp.</p>
+        </div>
+
+      <Footer />
     </div>
   )
 }
 
 export default HomeScreen;
+
+
+
+
+
