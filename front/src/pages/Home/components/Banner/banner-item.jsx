8962393809915d21e94eb924a4c091d566227840
Arbusta <iara.gerardi@arbusta.net>
2022-09-07 12:42:44 -0300

    estilos banner

--- front/src/pages/Home/components/Banner/banner-item.jsx
+++ front/src/pages/Home/components/Banner/banner-item.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 export function BannerItem({ news , index }) {
   return (
-    <li className="carrouselItem" key={index.toString()}>
+    <li className="bannerItem" key={index.toString()}>
         <div className="contentItemBanner">
           <h2>{news.title}</h2>
-          <p>{news.description}</p>
         </div>
         <img src={news.bgImage} alt={news.title}/>
     </li>
--- front/src/pages/Home/components/Banner/banner.jsx
+++ front/src/pages/Home/components/Banner/banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import news from "./news.json";
 import { BannerItem } from "./banner-item";
+import Icon from "../../../global-components/Svg-icon";
 import "./banner.css"
 export function Banner() {
     const [item, setItem] = useState(0);
@@ -9,16 +10,20 @@ export function Banner() {
 
     const itemLess = () => { item < 1 ? setItem(news.length - 1) : setItem(item - 1) }
     const itemPlus = () => { item >= news.length - 1 ? setItem(0) : setItem(item + 1) }
-    // let a = setInterval(sumarItem, 2000);
+    // let timer = setInterval(sumarItem, 2000);
 
     return (
         <div className="banner">
             <div className="buttonsBanner">
                 <div onClick={itemLess} className="arrow left">
-                    <span className="iconify buttonLeft" data-icon="akar-icons:arrow-left" />
+                    <Icon type="leftArrow"
+                        width="20"
+                        height="20" />
                 </div>
                 <div onClick={itemPlus} className="arrow right">
-                    <span className="iconify buttonRight" data-icon="akar-icons:arrow-right" />
+                    <Icon type="rightArrow"
+                        width="20"
+                        height="20" />
                 </div>
             </div>
             <ul className="bannerUL">{bannerItems[item]}</ul>
