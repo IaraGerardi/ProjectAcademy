cd535b81549710f8692e14f4018bca8ba3c9ee4b
Macarena-Leiva <macarena.leiva@arbusta.net>
2022-09-07 11:55:37 -0300

    ubicacion de btns

--- front/src/pages/Home/components/Proposal/Propuesta.jsx
+++ front/src/pages/Home/components/Proposal/Propuesta.jsx
@@ -4,22 +4,22 @@ function Proposal() {
     <div className="containerProposal">
        <div className="boxProposalText">
             <h2>¿Cual es nuestra propuesta?</h2>
-            <p>Programas de desarrollo profesional y descubrimiento.</p>
+            <p className='pProposal'>Programas de desarrollo profesional y descubrimiento.</p>
        </div>
         <div className="boxsProposalChildren">
             <div className="proposalBox ">
               <h3>Orientación vocacional</h3>
-              <p>Te entrenamos en la toma de decision tu trayecto profesional.</p>
+              <p className='pProposal'>Te entrenamos en la toma de decision tu trayecto profesional.</p>
               <button className='btnInfo'>Mas informacion</button>
             </div>
             <div className="proposalBox ">
               <h3>Re-orientacion vocacional</h3>
-              <p>Reinversion personal, laboral y profesional.</p>
+              <p className='pProposal'>Reinversion personal, laboral y profesional.</p>
               <button className='btnInfo'>Mas informacion</button>
             </div>
             <div className="proposalBox ">
               <h3>Espacios de aprendizaje</h3>
-              <p>Aprende a pensar desde otra logica.</p>
+              <p className='pProposal'>Aprende a pensar desde otra logica.</p>
               <button className='btnInfo'>Mas informacion</button>
             </div>
         </div>
