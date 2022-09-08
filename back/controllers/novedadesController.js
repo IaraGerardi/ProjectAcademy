994b5293f11cbfefdc7fd0b197ae0f8d066ede23
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-09-08 12:41:20 -0300

    controllers novedad y orientados

---
+++ back/controllers/novedadesController.js
@@ -0,0 +1,11 @@
+const ModelNovedades = require("../database/models/ModelNovedades.js");
+
+
+const getAllNovedades = async (req, res) => {
+    try {
+        const novedades = await ModelNovedades.findAll();
+        res.json(novedades)
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
---
+++ back/controllers/orientadoController.js
@@ -0,0 +1,15 @@
+const { ModelOrientado } = require("../database/associations");
+/* llama a modelo orientado desde associations 
+porque son los ultimos cambios que recibe, 
+en caso de no funcionar probar llamando al modelo
+desde "../database/models/ModelOrientado.js*/
+
+
+const getAllOrientados = async (req, res) => {
+    try {
+        const orientados = await ModelOrientado.findAll();
+        res.json(orientados)
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
