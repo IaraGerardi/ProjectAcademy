b0bee1b71fd9f2a3240c4a555286fbae530f921b
SebastianAvila00 <sebastian.avila@arbusta.net>
2022-09-08 14:15:34 -0300

    retoques en orientados

--- front/src/pages/OrientadosPage.jsx
+++ front/src/pages/OrientadosPage.jsx
@@ -1,6 +1,7 @@
 import HeaderInicio from "../Header-MenuLateral/HeaderInicio";
 import Sidebar from "../Header-MenuLateral/Sidebar";
 import "../css/orientados.css"
+import buscador from "../icons/logo-buscador.svg"
 
 function Orientados() {
     return (
@@ -15,13 +16,15 @@ function Orientados() {
                 <button className="btn-ingresar-orientado">Ingresar orientado</button>
             </div>
 
+
             <input className="buscador-orientado" type="text" placeholder="Buscar orientado por nombre y apellido" />
 
+            <img className="logo-buscador" src={buscador} alt="logo buscador" />
 
 
 
 
-        </div>
+        </div >
     );
 }
 
