cb04fd289d9d09c7cf36f8a3c427b069793886c0
goncataldo <gonzalo.cataldo@arbusta.net>
2022-09-12 17:51:38 -0300

    header con usuario logueado

--- front/src/Header-MenuLateral/HeaderInicio.jsx
+++ front/src/Header-MenuLateral/HeaderInicio.jsx
@@ -1,24 +1,52 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 
 function HeaderInicio() {
 
+    const [infoAdmin, setInfoAdmin] = useState([]);
 
+    const usuario = localStorage.getItem('usuario')
+    const parseado = JSON.parse(usuario)
 
+    useEffect(() => {
+        const getAdmin = async () => {
+            try {
+                const resInfoAdmin = await axios.get(`http://localhost:8000/admin/profile/${usuario.id}`);
+                setInfoAdmin(resInfoAdmin.data);
+                console.log(usuario);
+                console.log(parseado)
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getAdmin();
+    }, []);
 
-    
     return (
         <div className='header-inicio'>
 
-            {/* Esto habría que hacerlo dinamico y traiga la info de back */}
 
-            <p className='Title-inicio'>Bienvenido, Juan</p>
+            <div className='header-inicio'>
 
-            <div className='usuario-inicio'></div>
 
-            {/* <img className='usuario-inicio' src="" alt="usuario" /> */}
+                <p className='Title-inicio'>Bienvenido {parseado.name}</p>
+
+                <img className="usuario-inicio" src={require(`../img-back/admins/${parseado.avatar}`)} />
+
+            </div>
 
         </div>
+
+        // <div className='header-inicio'>
+        //     <li></li>
+
+        //     <p className='Title-inicio'>Bienvenido {resPhotoAdmin.name}</p>
+
+        //     <img className="usuario-inicio" src={require(`../img-back/orientados/${resPhotoAdmin[1]}`)} alt="Foto perfil admin" /> 
+
+        // </div>
 
     )
 
