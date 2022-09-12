50f26e6709edd3a3369d48826ac104fa6ef41f81
maidanaVa <veronica.maidana@arbusta.net>
2022-09-12 16:07:39 -0300

    get admin profile

--- front/src/pages/ProfileAdmin/componentes-Profile/CardProfile.jsx
+++ front/src/pages/ProfileAdmin/componentes-Profile/CardProfile.jsx
@@ -1,28 +1,31 @@
 
 import { useEffect, useState } from 'react'
-import imgAdmin from '../assets/imgUser.jpg'
+
 import axios from 'axios';
 import './CardProfile.css';
 
 function CardProfile() {
      const [admin,setAdmin]=useState([]);//estado donde voy a guardar el objeto del admin  y luego obtener sus datos a traves de la notacion de puntos
+     
+     const URI=`http://localhost:8000/admin/profile/1`;
 
-    const getAdminProfile= async(adminNumber)=>{
+    const getAdminProfile= async()=>{
         try{
-            const resAdmin = await axios (`http://jsonplaceholder.typicode.com/users/${adminNumber}`)
-            const resAdminData=resAdmin.data; 
+            const resAdmin = await axios.get (URI)
+            setAdmin(resAdmin.data); 
           
-            setAdmin(resAdminData);// actualiza el estado dinamicamente segun el valor del parametro que le pases
-        
+           
+           
+
         }catch(error){// en caso de fallar 
             console.log (error)
         }
     }
     useEffect( ()=>{ // la a ejecutar la funcion luego de renderizar la pantalla y no todo el tiempo
        
-        getAdminProfile(5);
+        getAdminProfile(); 
       },[])
-   // donde le voy a pasar el valor del parametro a la funcion 
+  console.log(localStorage)
 
   return ( // hay etiquetas que tienen clses de tailwind las dejo ahi para que si nos ponemos de acuerdo podamos usarlo.
     <div className="containerCardProfile">
@@ -31,7 +34,7 @@ function CardProfile() {
 
        <div className='containerAdminProfile w-3/6 h-3/5 p-8 bg-white flex flex-row rounded-lg'>
                 <div className="boxImgAdminProfile w-5/12 flex justify-center">
-                       <img className="imgAdmin w-36 h-36 mt-8 rounded-full" src={imgAdmin} alt="" />
+                       <img className="imgAdmin w-36 h-36 mt-8 rounded-full" src={require(`../../../img-back/admins/${admin.avatar}`)} alt="" />
                 </div>
 
                 
@@ -53,7 +56,7 @@ function CardProfile() {
 
                         <div className="linkedInAdminProfile">
                             <span className='text-slate-400'>LIKED IN</span>
-                            <p>{admin.username}</p>
+                            <p>{admin.linkedin}</p>
                         </div>
          
                 </div>
