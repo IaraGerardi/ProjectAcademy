c49ebd981494a4494102550db941dc4d9cd8e11b
SebastianAvila00 <sebastian.avila@arbusta.net>
2022-09-12 09:58:26 -0300

    pagina eventos

--- front/src/pages/EventPage.jsx
+++ front/src/pages/EventPage.jsx
@@ -1,13 +1,32 @@
+import HeaderInicio from "../Header-MenuLateral/HeaderInicio.jsx";
+import Sidebar from "../Header-MenuLateral/Sidebar.jsx";
+
 
 function Eventos() {
-    return (
-        <div>
-    
+  return (
+    <>
+            <div className="cont-major">
+
+                <div className="menu-sa"><Sidebar /></div>
+
+                <div className="header-sa">
+                    <div className="sa-header-children"><HeaderInicio /></div>
+
+                    <div className="sa-seccion">
 
 
-        </div>
 
-    );
+
+                    </div>
+                </div>
+
+            </div>
+
+
+        </>
+
+
+  );
 }
 
 export default Eventos;
