687b6b5f6346f03fae78a08add08eb8b7c7a82ea
Arbusta <iara.gerardi@arbusta.net>
2022-09-14 10:09:52 -0300

    agrego icono

--- front/src/pages/global-components/Svg-icon.jsx
+++ front/src/pages/global-components/Svg-icon.jsx
@@ -14,7 +14,11 @@ function Icon({ type, height, width, classname }) {
                                 <path xmlns="http://www.w3.org/2000/svg" d="M12,5a1,1,0,0,0-1,1v8a1,1,0,0,0,2,0V6A1,1,0,0,0,12,5Z" />
                                 <rect xmlns="http://www.w3.org/2000/svg" x="11" y="17" width="2" height="2" rx="1" />
                             </>
-                            : null
+                            : type === "userIcon" ?
+                                <g xmlns="http://www.w3.org/2000/svg" id="_01_align_center" data-name="01 align center">
+                                    <path d="M21,24H19V18.957A2.96,2.96,0,0,0,16.043,16H7.957A2.96,2.96,0,0,0,5,18.957V24H3V18.957A4.963,4.963,0,0,1,7.957,14h8.086A4.963,4.963,0,0,1,21,18.957Z" /><path d="M12,12a6,6,0,1,1,6-6A6.006,6.006,0,0,1,12,12ZM12,2a4,4,0,1,0,4,4A4,4,0,0,0,12,2Z" />
+                                </g>
+                                : null
                 }
             </svg>
         </>
