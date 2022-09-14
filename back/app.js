628c547cfe8c51f4a12460086b22f0b3a597edfe
Cristian Vera <cristian.vera@arbusta.net>
2022-09-14 10:27:05 -0300

    bd

--- back/app.js
+++ back/app.js
@@ -42,7 +42,7 @@ app.listen(PORT, () => {
     try {
         sequelize.authenticate();
         //true = rompe y crea la base de datos - false = queda inactivo
-        sequelize.sync({force: true});
+        // sequelize.sync({force: true});
         console.log(`Database conected`);
     } catch (error) {
         console.log(error);
