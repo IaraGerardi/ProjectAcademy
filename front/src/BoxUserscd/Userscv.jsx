183dbe0f8aa5468992da4e303266ed3f6283f823
Cristian Vera <cristian.vera@arbusta.net>
2022-09-14 10:25:50 -0300

    Base de datos

--- front/src/BoxUserscd/Userscv.jsx
+++ front/src/BoxUserscd/Userscv.jsx
@@ -2,33 +2,27 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Userscvcss.css";
 
-
-function Userscv () {
+function Userscv() {
   const [newusers, setNewusers] = useState([]);
-
-  // console.log(newusers);
-
-  //PEDIDO ASINCRONO
 
   useEffect(() => {
     const getOrientados = async () => {
       try {
         const res = await axios.get("http://localhost:8000/admin/orientados");
         setNewusers(res.data);
-  
+
         console.log(res.data);
       } catch (error) {
         console.log(error);
       }
     };
+
     getOrientados();
   }, []);
 
   return (
     <div className="box-cont">
       <ul>
-        {/* {newusers.length === 0 && <p>Cargando...</p>}
-        {newusers.map((Usersapi, i) => { */}
         {newusers.length === 0 && <p>Cargando...</p>}
         {newusers.map((Usersapi, i) => {
           return (
@@ -38,12 +32,11 @@ function Userscv () {
                   <li key={i}>
                     <div className="boxflex">
                       <div>
-                        <img className="ImgUsers" src={require(`../img-back/orientados/${Usersapi.photoProfile}`)} alt="" />
-                        {/* <img
+                        <img
                           className="ImgUsers"
-                          src=
-                          alt="Foto Usuario"
-                        /> */}
+                          src={require(`../img-back/orientados/${Usersapi.photoProfile}`)}
+                          alt=""
+                        />
                       </div>
 
                       <div>
@@ -52,15 +45,11 @@ function Userscv () {
                         </h4>
                         <p>{Usersapi.school}</p>
                       </div>
-
-                      {/* <div className="classicon"><img className="usericon" src={require('../usuarios/img/user.png')} alt="usuario" /></div> */}
                     </div>
                   </li>
                 </div>
               </div>
             </>
-
-            // </div>
           );
         })}
       </ul>
--- front/src/UsersAdmin/UserAdmin.jsx
+++ front/src/UsersAdmin/UserAdmin.jsx
@@ -2,20 +2,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./UserAdmin.css";
 
-
-function UserAdmin () {
+function UserAdmin() {
   const [newusers, setNewusers] = useState([]);
-
-  // console.log(newusers);
-
-  //PEDIDO ASINCRONO
 
   useEffect(() => {
     const getOrientados = async () => {
       try {
         const res = await axios.get("http://localhost:8000/admin/orientados");
         setNewusers(res.data.slice(-6));
-  
+
         console.log(res.data);
       } catch (error) {
         console.log(error);
@@ -27,8 +22,6 @@ function UserAdmin () {
   return (
     <div className="box-cont">
       <ul>
-        {/* {newusers.length === 0 && <p>Cargando...</p>}
-        {newusers.map((Usersapi, i) => { */}
         {newusers.length === 0 && <p>Cargando...</p>}
         {newusers.map((Usersapi, i) => {
           return (
@@ -38,12 +31,11 @@ function UserAdmin () {
                   <li key={i}>
                     <div className="boxflex">
                       <div>
-                        <img className="ImgUsers" src={require(`../img-back/orientados/${Usersapi.photoProfile}`)} alt="" />
-                        {/* <img
+                        <img
                           className="ImgUsers"
-                          src=
-                          alt="Foto Usuario"
-                        /> */}
+                          src={require(`../img-back/orientados/${Usersapi.photoProfile}`)}
+                          alt=""
+                        />
                       </div>
 
                       <div>
@@ -52,15 +44,11 @@ function UserAdmin () {
                         </h4>
                         <p>{Usersapi.school}</p>
                       </div>
-
-                      {/* <div className="classicon"><img className="usericon" src={require('../usuarios/img/user.png')} alt="usuario" /></div> */}
                     </div>
                   </li>
                 </div>
               </div>
             </>
-
-            // </div>
           );
         })}
       </ul>
