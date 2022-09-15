2ec45285027caaa53204bacaa98884a8cd19051d
SebastianAvila00 <sebastian.avila@arbusta.net>
2022-09-15 10:30:22 -0300

    logout completo

--- front/src/pages/sidebar-header/components/HeaderInicio.jsx
+++ front/src/pages/sidebar-header/components/HeaderInicio.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import LogoPerfil from "../icons/logo-perfil.svg"
 import cerrarSesion from "../icons/logo-cerrar.svg"
+
 
 
 function HeaderInicio() {
 
     const [infoAdmin, setInfoAdmin] = useState([]);
     const [active, setActive] = useState(false);
-
+    const navigate = useNavigate()
     const usuario = localStorage.getItem('usuario')
     const parseado = JSON.parse(usuario)
 
@@ -31,19 +32,18 @@ function HeaderInicio() {
 
 
 
-    useEffect(() => {
+
         const getLogout = async () => {
             try {
-                const resLogout = await axios.get(`http://localhost:8000/logout`);
+                const resLogout = await axios.get(`http://localhost:8000/logout`,{withCredentials: true});
                 setInfoAdmin(resLogout.data);
                 console.log(resLogout.data);
-                
+                navigate('/login')
             } catch (error) {
                 console.log(error);
             }
         };
-        getLogout();
-    }, []);
+
 
 
 
@@ -59,11 +59,11 @@ function HeaderInicio() {
             </div>
 
             <div className={`sesion ${active ? 'mostrar-sesion' : 'ocultar-sesion'}`}>
-                <Link to="/profile"><img  className="logo-perfil" src={LogoPerfil} alt={parseado.name} /> Mi perfil</Link>
+                <Link to="/profile"><img  className="logo-perfil" src={LogoPerfil}/> Mi perfil</Link>
 
 
-                <Link to="/"><img className="logo-perfil" src={cerrarSesion} alt={parseado.name} /> Cerrar Sesión</Link>
-
+                <Link to="/"><img className="logo-perfil" src={cerrarSesion}/><button onClick={()=>{getLogout()}}>Cerrar sesión</button></Link>
+                
 
             </div>
 
@@ -74,4 +74,3 @@ function HeaderInicio() {
 }
 
 export default HeaderInicio;
-
