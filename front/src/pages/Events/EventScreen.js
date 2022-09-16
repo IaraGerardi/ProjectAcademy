70c6ded834314f691172460daa4c5306d495dbb9
Macarena-Leiva <macarena.leiva@arbusta.net>
2022-09-16 14:37:35 -0300

    links sidebar

--- front/src/pages/Events/EventScreen.js
+++ front/src/pages/Events/EventScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeaderInicio from '../sidebar-header/components/HeaderInicio'
-import { Sidebar } from '../sidebar-header/components/Sidebar'
+import { Sidebar } from '../sidebar-header/components/Sidebar.js'
 
 export const EventScreen = () => {
   return (
--- front/src/pages/sidebar-header/components/Sidebar.js
+++ front/src/pages/sidebar-header/components/Sidebar.js
@@ -4,6 +4,7 @@ import logo from '../icons/logo-valtech.svg'
 import inicio from '../icons/logo-inicio.svg'
 import orientado from '../icons/logo-orientado.svg'
 import calendar from '../icons/logo-calendar.svg'
+import { Link } from 'react-router-dom'
 
 export const Sidebar = () => {
   return (
@@ -15,18 +16,21 @@ export const Sidebar = () => {
             </div>
 
             <ul className='ulSidebar'>
-                <li className='liSidebar'>          
+                <Link to='/inicio' className='linkSidebar'><li className='liSidebar'>          
                     <img className="iconSidebar" src={inicio} alt="inicio" />
                     <p className='pSidebar'>Inicio</p>
                 </li>
-                <li className='liSidebar'>                     
+                </Link>
+                <Link to='/orientados' className='linkSidebar'><li className='liSidebar'>                     
                     <img className="iconSidebar" src={orientado} alt="orientados" />
                     <p className='pSidebar'>Orientados</p>
                 </li>
-                <li className='liSidebar'>
+                </Link>
+                <Link to='/eventos' className='linkSidebar'><li className='liSidebar'>
                     <img className="iconSidebar" src={calendar} alt="calendar" />
                     <p className='pSidebar'>Eventos</p>
                 </li>
+                </Link>
             </ul>
         
         </div>
