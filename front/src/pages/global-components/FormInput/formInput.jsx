03cd57a8e91a46f4729cebf1aa1c65e53fa2e7bc
Arbusta <iara.gerardi@arbusta.net>
2022-09-19 14:41:41 -0300

    codigo comentado

--- front/src/pages/global-components/FormInput/formInput.jsx
+++ front/src/pages/global-components/FormInput/formInput.jsx
@@ -2,19 +2,22 @@ import Icon from "../Svg-icon";
 import "./formInput.css"
 
 function FormInput(props) {
-
+    // Props de valores del input
     const { id, type, label, placeholder } = props
+    // Prop con el estado de la verificacion del input y prop con la funcion que guarda el valor del mismo
     const { verifyInput, handleChange } = props
 
     return (
         <>
             <label className="p-2.5 items-center" htmlFor={id}>{label}</label>
+            {/* Si el valor del prop que tiene las validaciones no tiene ningun error el input tiene un borde gris y de 0.3 pixeles, si hay algun error el borde cambia a un color rojo*/}
             <input className={`w-80 ${verifyInput !== null && verifyInput !== true ? "border border-solid border-red-600" : "formInput"}`}
                 id={id}
                 name={id}
                 type={type}
                 onChange={handleChange}
                 placeholder={placeholder} />
+            {/* Si el input no tiene ningun error no se muestra nada, si hay algun error se renderiza el div con el mensaje de error y un icono */}
             {verifyInput !== null && verifyInput !== true ?
                 <div className="flex ml-2.5">
                     <Icon
