b8bc94270164899221ebb235b6055bcad9ca651e
goncataldo <gonzalo.cataldo@arbusta.net>
2022-09-19 17:45:43 -0300

    cambios en orientado controller y admin router

--- back/controllers/orientadoController.js
+++ back/controllers/orientadoController.js
@@ -1,5 +1,5 @@
 const bcryptjs = require('bcryptjs');
-const { ModelOrientado } = require("../database/associations");
+const { ModelOrientado, ModelOrientador } = require("../database/associations");
 /* llama a modelo orientado desde associations 
 porque son los ultimos cambios que recibe, 
 en caso de no funcionar probar llamando al modelo
@@ -11,18 +11,18 @@ const getAllOrientados = async (req, res) => {
         const orientados = await ModelOrientado.findAll();
         res.json(orientados)
     } catch (error) {
-        res.json({message: error.message})
+        res.json({ message: error.message })
     }
 }
 
-const createOrientado = async(req, res) => {
-    const {name, lastname, email, phone, program} = req.body;
-    const { dni, age, school, address, why} = req.body;
+const createOrientado = async (req, res) => {
+    const { name, lastname, email, phone, program } = req.body;
+    const { dni, age, school, address, why } = req.body;
     const photoProfile = req.files[0] ? req.files[0].filename : null
     const password = await bcryptjs.hash(req.body.password, 10);
 
     try {
-        const user = await ModelOrientado.create({ 
+        const user = await ModelOrientado.create({
             name, //Cuando el nombre de la propiedad es la misma no es necesario poner name: name.
             lastname,
             email,
@@ -38,13 +38,41 @@ const createOrientado = async(req, res) => {
             dni,
             password
         });
-        res.json(user) 
+        res.json(user)
     } catch (error) {
-        res.json({message: error.message})
+        res.json({ message: error.message })
     }
+}
+
+const orientadoById = async (req, res) => {
+    const orientado = await ModelOrientado.findOne(
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+    res.json(orientado)
+}
+
+const orientadores = async (req,res)=>{
+    const orientadores = await ModelOrientador.findAll();
+    res.json(orientadores)
+}
+
+const orientadorToOrientado = async (req, res) => {
+    await ModelOrientado.update({
+        OrientadoreId: req.body.orientador
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
 }
 
 module.exports = {
     createOrientado,
-    getAllOrientados    
+    getAllOrientados,
+    orientadorToOrientado,
+    orientadoById,
+    orientadores
 }
--- back/routes/adminRouter.js
+++ back/routes/adminRouter.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const routerAdmin = express.Router();
-const { getAllOrientados, createOrientado } = require ("../controllers/orientadoController.js");
+const { getAllOrientados, createOrientado, orientadoById, orientadores, orientadorToOrientado } = require ("../controllers/orientadoController.js");
 const { getAllNovedades } = require("../controllers/novedadesController.js");
 const { isAuthenticated } = require("../controllers/loginAdminController.js"); //Autenticacion para que solo puedan ingresar usuarios logueados (admins)
 const { getAllAdminsProfiles, getAdminProfile } = require("../controllers/profileAdminController.js");
@@ -16,5 +16,17 @@ routerAdmin.get('/admin/profile/:id', getAdminProfile);
 
 //Ruta para crear Orientados
 routerAdmin.post('/admin/create', userImage.any(), createOrientado);
+
+//Ruta para mostrar Orientado
+routerAdmin.get('/admin/orientados/:id', orientadoById)
+
+//Ruta para mostrar Orientado y su Orientador
+routerAdmin.get('/admin/orientados/:id/orientador', orientadoById, orientadores)
+
+//Ruta para asignar o modificar Orientador a Orientado
+routerAdmin.put('/admin/orientados/:id/orientadorToOrientado', orientadorToOrientado)
+
+
+
 
 module.exports = routerAdmin;
