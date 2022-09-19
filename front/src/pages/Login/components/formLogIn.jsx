b83854c809862fb49e23424987a3a6c2e3e83570
Arbusta <iara.gerardi@arbusta.net>
2022-09-19 16:34:45 -0300

    codigo comentado

--- front/src/pages/Login/components/formLogIn.jsx
+++ front/src/pages/Login/components/formLogIn.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import FormInput from "../../global-components/FormInput/formInput";
+import FormInput from "../../global-components/formInput";
 import axios from "axios";
+import { useEffect } from "react";
 
 function FormLogIn() {
     const URI = "http://localhost:8000/login"
     const navigate = useNavigate();
+    let isVerified = false;
 
-    const [form, setForm] = useState({
-        emailLog: null,
-        passwordLog: null,
-    })
-    const [verifyForm, setVerifyForm] = useState({
-        emailLog: null,
-        passwordLog: null,
-    })
+    const [form, setForm] = useState({ emailLog: null, passwordLog: null, })
+    const [verifyForm, setVerifyForm] = useState({ emailLog: null, passwordLog: null, })
 
+    // Funcion para verificar inputs, se llama una vez por cada componente que necesite ser verificado
     const verifyInput = (id) => {
         form[id] === null || form[id] === "" ? setVerifyForm(prevVerifyForm => ({
             ...prevVerifyForm,
@@ -27,7 +24,7 @@ function FormLogIn() {
             })) :
                 setVerifyForm(prevVerifyForm => ({
                     ...prevVerifyForm,
-                    [id]: true
+                    [id]: true,
                 }))
     }
 
@@ -43,23 +40,38 @@ function FormLogIn() {
         verifyInput("passwordLog");
     }
 
+    // Metodo para verificar si el formulario esta validado, esta dentro de un useEffect para ver si soluciona lo comentado mas abajo en la funcion handleSubmit
+    useEffect(() => {
+        console.log("verifyForm en un useEffect:")
+        console.log(verifyForm)
+        isVerified = Object.values(verifyForm).every(value => {
+            if (value === true) {
+                return true;
+            }
+            return false;
+        });
+    }, [verifyForm])
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        verifyLogIn(); // Lo ejecuta para ver si no hay errores y corta la funcion, cuando no hay errores (despues de que si los haya habido) desaparece el error peeeero no ejecuta el resto
-        if (verifyForm.emailLog === true && verifyForm.passwordLog === true) {
-            await axios.post(`${URI}`, form, { withCredentials: true })
-                .then((response) => {
-                    if (response.data.si) {
-                        localStorage.setItem("usuario", JSON.stringify(response.data.admin));
-                        navigate('/inicio')
-                    } else {
-                        setVerifyForm({
-                            ...verifyForm,
-                            [response.data.input]: response.data.alertMessage,
-                        })
-                    }
-                })
-        }
+        verifyLogIn();
+
+        await axios.post(`${URI}`, form, { withCredentials: true })
+            .then((response) => {
+                console.log("verifyForm adentro del .then:")
+                console.log(verifyForm)
+                /*console.log("isVerified adentro del .then:")
+                console.log(isVerified)*/
+                if (response.data.si) {
+                    localStorage.setItem("id", JSON.stringify(response.data.admin.id));
+                    navigate('/inicio');
+                    // En esta parte tengo problemas, tanto el estado como el isVerified tienen valores desactualizados dentro de la funcion, eso causa que si hay algun error tenga que hacer click dos veces en el boton submit para que el else if funcione
+                } else if (isVerified) {
+                    setVerifyForm({
+                        ...verifyForm, [response.data.input]: response.data.alertMessage,
+                    })
+                }
+            })
     }
 
     return (
