abb2e6aa8fea981bc1de4ddf86abb6d2b084af29
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-09-28 10:12:25 -0300

    Comentarios a asociaciones OrientadoEvento

--- back/database/associations.js
+++ back/database/associations.js
@@ -2,12 +2,15 @@ const ModelOrientado = require("./models/ModelOrientado");
 const ModelOrientador = require("./models/ModelOrientador");
 const ModelEvento = require("./models/ModelEvento.js");
 
+//1 a N / Orientadores a Orientados
 ModelOrientador.hasMany(ModelOrientado); //Crea FK OrientadoreId para los orientados
 ModelOrientado.belongsTo(ModelOrientador);//Cada orientado tiene el ID de su unico orientador
 
-ModelOrientador.hasMany(ModelEvento); //Crea FK orientadoreId para los orientados
-ModelEvento.belongsTo(ModelOrientador);//Cada evento tiene el ID de su unico orientador
+//1 a N / Orientador a Eventos
+ModelOrientador.hasMany(ModelEvento); //
+ModelEvento.belongsTo(ModelOrientador); //
 
+//N a N / Orientados a Eventos
 ModelOrientado.belongsToMany(ModelEvento, {through: "orientado_evento"}); // Tabla pivot llamada "orientado_evento", relacion tabla orientado y tabla Evento
 ModelEvento.belongsToMany(ModelOrientado, {through: "orientado_evento"});
 
