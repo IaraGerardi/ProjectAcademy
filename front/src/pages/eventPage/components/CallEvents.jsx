667a7bb9eb5b42cb75e1b8411431f576fd7613f3
SebastianAvila00 <sebastian.avila@arbusta.net>
2022-09-29 13:05:34 -0300

    correccion de errores

--- front/src/pages/eventPage/components/CallEvents.jsx
+++ front/src/pages/eventPage/components/CallEvents.jsx
@@ -3,82 +3,88 @@ import "./call-events.css"
 import Delete from "./img/delete.svg"
 
 
+
 function CallEvents() {
+
+
+
+
+
+
     return (
+        // TABLA DE EVENTOS
         <>
             <div className="table-events">
                 <div className="container-events">
-                    <form action="">
-                        <table >
-                            <tr className="title-table">
-                                <td className="title-events"><strong>Fecha</strong></td>
-                                <td className="title-events"><strong>Horario</strong></td>
-                                <td className="title-events"><strong>Eventos</strong></td>
-                                <td className="title-events"><strong>Participantes</strong></td>
-                            </tr>
 
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
+                    <table >
+                        <tr className="title-table">
+                            <td className="title-events"><strong>Fecha</strong></td>
+                            <td className="title-events"><strong>Horario</strong></td>
+                            <td className="title-events"><strong>Eventos</strong></td>
+                            <td className="title-events"><strong>Participantes</strong></td>
+                        </tr>
 
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Maximiliano Ivan Portel Beraud </td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Avila Sebastian</td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Avila Sebastian</td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Avila Sebastian</td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Avila Sebastian</td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
 
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
-                            <tr>
-                                <td className="events">28/09/2022</td>
-                                <td className="events">19:30hs</td>
-                                <td className="events">Evento grupal</td>
-                                <td className="events">Avila Sebastian</td>
-                                <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
-                            </tr>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Avila Sebastian</td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
 
-                        </table>
-                    </form>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Avila Sebastian</td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
+                        <tr>
+                            <td className="events">28/09/2022</td>
+                            <td className="events">19:30hs</td>
+                            <td className="events">Evento grupal</td>
+                            <td className="events">Avila Sebastian</td>
+                            <td className="events left"><img className="icon-delete" src={Delete} alt="icon de tacho de basura" /></td>
+                        </tr>
 
+                    </table>
                 </div>
 
             </div>
