79db89896156e4b3f84c486c3c7f03e9b0585b17
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-09-29 16:13:04 -0300

    Imagen borrada en caso de fallar form, Validacion de foto en exp.val.

--- back/helpers/validateHelper.js
+++ back/helpers/validateHelper.js
@@ -1,15 +1,31 @@
 const { validationResult } = require('express-validator');
+const path = require('path');
+const fs = require('fs');
 /* 
 Se encarga de revisar los validationResult de express-Validator y seguir la cadena
 o de enviar todos los errores qeu haya en un array y cortar la cadena 
 */
+
+/* 
+validationResult trae los errores o viene vacio.
+-En caso estar vacio continua la cadena.
+-En caso de que no este vacio(osea existan errores):
+1° Revisa si existe un archivo(req.file), si existe lo elimina (En el if dentro del else).
+2° llegado a throw toma los errores de validationResutls y los usa como errores en el catch
+*/
 const validateResult = (req, res, next) => {
     try {
-        validationResult(req).throw();
-        return next();
+        if (validationResult(req).isEmpty()) {
+            return next();
+        } else {
+            if (req.file) {
+                fs.unlinkSync(path.join(__dirname, `../../front/src/img-back/orientados/${req.file.filename}`));
+            }
+            throw validationResult(req)
+        }
     } catch (error) {
         res.status(403);
-        res.send({errors: error.array()});
+        res.json(error);
     }
 };
 
