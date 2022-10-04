89c1ce6cdd1a48aaf7aacc29f6d2a02639a6a9b4
goncataldo <gonzalo.cataldo@arbusta.net>
2022-10-04 16:40:18 -0300

    autenticacion jwt

--- back/controllers/loginAdminController.js
+++ back/controllers/loginAdminController.js
@@ -1,124 +1,87 @@
 const jwt = require('jsonwebtoken')
 const ModelAdmin = require('../database/models/ModelAdmin.js')
-/* const bcryptjs = require('bcryptjs') */
-const {promisify} = require('util')
+const { promisify } = require('util')
 
 exports.adminLogin = async (req, res) => {
     try {
         const emailLog = req.body.emailLog
         const passwordLog = req.body.passwordLog
-        if(!emailLog && !passwordLog) {
+        if (!emailLog && !passwordLog) {
             res.json({
-                alert: true,
-                alertTitle: "Advertencia",
                 alertMessage: "Ingrese un email y una contraseña",
-                alertIcon: 'info',
-                showConfirmButton: true,
-                timer: false,
-                ruta: 'login',
             })
         } else if (!emailLog) {
             res.json({
-                alert: true,
-                alertTitle: "Advertencia",
                 alertMessage: "Ingrese un email",
-                alertIcon: 'info',
-                showConfirmButton: true,
-                timer: false,
-                ruta: 'login',
                 input: "emailLog"
             })
-        } else if(!passwordLog) {
+        } else if (!passwordLog) {
             res.json({
-                alert: true,
-                alertTitle: "Advertencia",
                 alertMessage: "Ingrese una contraseña",
-                alertIcon: 'info',
-                showConfirmButton: true,
-                timer: false,
-                ruta: 'login',
                 input: "passwordLog"
             })
         } else {
-            const admin = await ModelAdmin.findAll({
-                where: {email:emailLog}
-                })
-                console.log(admin)
+            const admin = await ModelAdmin.findOne({
+                where: { email: emailLog }
+            })
             if (admin.length == 0) {
                 res.json({
-                    alert: true,
-                    alertTitle: "Error",
                     alertMessage: "Email incorrecto",
-                    alertIcon: 'error',
-                    showConfirmButton: true,
-                    timer: false,
-                    ruta: 'login',
                     input: "emailLog"
                 })
-            } else if(passwordLog !== admin[0].password){
+            } else if (passwordLog !== admin.password) {
                 console.log(admin)
-                console.log(admin[0])
-                console.log(admin[0].password)
+                console.log(admin)
+                console.log(admin.password)
                 console.log(passwordLog)
                 res.json({
-                    alert: true,
-                    alertTitle: "Error",
                     alertMessage: "Contraseña incorrecta",
-                    alertIcon: 'error',
-                    showConfirmButton: true,
-                    timer: false,
-                    ruta: 'login',
                     input: "passwordLog"
                 })
-            }else {
-                const id = admin[0].id
-                const token = jwt.sign({ id: id }, process.env.JWT_SECRETO, {
-                    expiresIn: process.env.JWT_TIEMPO_EXPIRA
+            } else {
+                const loggedAdmin = await ModelAdmin.findOne({
+                    where: { email: emailLog },
+                    attributes: {exclude: ['password']}
                 })
-                console.log("TOKEN: " + token + " para el USUARIO : " + admin[0].user)
+                const id = admin.id
+                const token = jwt.sign({ id: id }, process.env.JWT_SECRET, {
+                    expiresIn: 24 * 60 * 60 * 1000
+                })
+                console.log("TOKEN: " + token + " para el USUARIO : " + admin.user)
                 const cookiesOptions = {
-                    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+                    expires: new Date(Date.now() + 24 * 60 * 60 * 1000), //La cookie expira en 24 horas
                     httpOnly: true
                 }
                 res.cookie('jwt', token, cookiesOptions) // cookieOptions me tira que expires es invalido, revisar más adelante (Solucionado colocando "JWT_COOKIE_EXPIRES =" en el .env)
                 res.json({
-                    alert: true,
-                    alertTitle: "Conexión exitosa",
                     alertMessage: "¡LOGIN CORRECTO!",
-                    alertIcon: 'success',
-                    showConfirmButton: false,
-                    timer: 800,
-                    ruta: '',
-                    si:'si',
-                    token: token,
-                    cookiesOptions: cookiesOptions,
-                    admin: admin[0]
+                    si: 'si',
+                    admin: loggedAdmin
                 });
             }
         }
-        } catch (error) {
+    } catch (error) {
         console.log(error)
     }
 }
 
-
 exports.isAuthenticated = async (req, res, next) => {
+    console.log(`el req de cookie jwt: ${req.cookies.jwt}`)
     if (req.cookies.jwt) {
         try {
-            const decodificada = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRETO)
+            const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
             const admin = await ModelAdmin.findAll({
-                where: { id: decodificada.id }
+                where: { id: decoded.id }
             })
             if (!admin) { return next() }
             req.admin = admin[0]
             return next()
-
         } catch (error) {
             console.log(error)
             return next()
         }
     } else {
-            res.json({mensaje: 'Usuario no logueado, porfavor inicie sesion'})
+        res.json({ logged: 'Not logged' })
     }
 }
 
--- back/routes/adminRouter.js
+++ back/routes/adminRouter.js
@@ -5,44 +5,45 @@ const routerAdmin = express.Router();
 const { createEvento, getEvent, deleteEvento } = require("../controllers/eventosController")
 const { getAllOrientados, createOrientado, orientadoById, getAllOrientadores, orientadorToOrientado, orientadoAndOrientador, getAllOrientados2 } = require ("../controllers/orientadoController.js");
 const { getAllNovedades } = require("../controllers/novedadesController.js");
-const { isAuthenticated } = require("../controllers/loginAdminController.js"); //Autenticacion para que solo puedan ingresar usuarios logueados (admins)
 const { getAllAdminsProfiles, getAdminProfile } = require("../controllers/profileAdminController.js");
+//Autenticacion para que solo puedan ingresar usuarios logueados (admins)
+const { isAuthenticated } = require("../controllers/loginAdminController.js"); 
 //Middlewares y Validaciones
 const photoProfileCheck = require("../middleware/orientadoImages.js");
 const validateCreate = require("../validations/createOrientado.js");
 const { eventValidator } = require("../validations/createEvento");
 
 //Rutas de la vista privada del Admin
-routerAdmin.get('/admin/orientados', getAllOrientados);
-routerAdmin.get('/admin/pruebaorientados', getAllOrientados2); //Es de prueba despues se borra
-routerAdmin.get('/admin/novedades', getAllNovedades);
+routerAdmin.get('/admin/orientados', isAuthenticated, getAllOrientados);
+routerAdmin.get('/admin/pruebaorientados', isAuthenticated, getAllOrientados2); //Es de prueba despues se borra
+routerAdmin.get('/admin/novedades', isAuthenticated, getAllNovedades);
 
 //Rutas para obtener profiles de admins
-routerAdmin.get('/admin/profile', getAllAdminsProfiles);
-routerAdmin.get('/admin/profile/:id', getAdminProfile);
+routerAdmin.get('/admin/profile', isAuthenticated,  getAllAdminsProfiles);
+routerAdmin.get('/admin/profile/:id', isAuthenticated, getAdminProfile);
 
 //Ruta para crear Orientados
-routerAdmin.post('/admin/create', photoProfileCheck, validateCreate, createOrientado);
+routerAdmin.post('/admin/create', isAuthenticated, photoProfileCheck, validateCreate, createOrientado);
 
 //Ruta para mostrar al Orientado que esta en la ID
-routerAdmin.get('/admin/orientados/:id', orientadoById)
+routerAdmin.get('/admin/orientados/:id', isAuthenticated, orientadoById)
 
 //Ruta para mostrar a todos los orientadores
-routerAdmin.get('/admin/orientadores', getAllOrientadores)
+routerAdmin.get('/admin/orientadores', isAuthenticated, getAllOrientadores)
 
 //Ruta para mostrar Orientado que esta en la ID y su Orientador
-routerAdmin.get('/admin/orientados/:id/orientador', orientadoAndOrientador)
+routerAdmin.get('/admin/orientados/:id/orientador', isAuthenticated, orientadoAndOrientador)
 
-//Ruta para asignar o modificar Orientador a Orientado
-routerAdmin.put('/admin/orientados/:id/orientadorToOrientado', orientadorToOrientado)
+//Ruta para asignar o modificar Orientador a Orientado 
+routerAdmin.put('/admin/orientados/:id/orientadorToOrientado', isAuthenticated, orientadorToOrientado)
 
 //Ruta para ver Eventos
-routerAdmin.get('/admin/event', getEvent)
+routerAdmin.get('/admin/event', isAuthenticated, getEvent)
 
 //Ruta para crear eventos
-routerAdmin.post('/admin/createEvent', eventValidator, createEvento)
+routerAdmin.post('/admin/createEvent', isAuthenticated, eventValidator, createEvento)
 
 //Ruta para eliminar eventos
-routerAdmin.delete('/admin/:id/deleteEvent', deleteEvento)
+routerAdmin.delete('/admin/:id/deleteEvent', isAuthenticated, deleteEvento)
 
 module.exports = routerAdmin;
