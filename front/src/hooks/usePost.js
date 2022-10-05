3aae83c08e1c62163414a04350ba87e5dbd9bad7
Arbusta <iara.gerardi@arbusta.net>
2022-10-05 09:12:09 -0300

    cambios en usePost

--- front/src/hooks/usePost.js
+++ front/src/hooks/usePost.js
@@ -1,26 +1,24 @@
 import { useState } from "react";
 import axios from "axios";
 
-function usePost(URI) {
+function usePost(URI, form, verifications) {
     const [backVerifications, setBackVerifications] = useState({});
     const [success, setSuccess] = useState(null);
     const [error, setError] = useState(null);
+
+    // For each verification it must add an object property whose value is null and key is the id of the currect index of the array
+    // , tiene que haber una funcion que se exporte
 
     const postForm = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post(URI, form, { withCredentials: true })
-            // Necesitaria que haya una propiedad estandar que tengan todos los resjson de las peticiones post, asi puedo centralizar cosas aca
-            if (response.data.si) {
+            if (response.status === 200) {
                 setSuccess(true)
             } else {
-                /*Si hay verificaciones del back que deban ser leidas por los usuarios podria manejar bien como se muestran 
-                si todas tienen una propiedad input con el id del input, y otra que se llame alertMessage, 
-                que tenga el mensaje que se le mostraria al usuario */
-                /* En caso de que haya que mostrar mas de un error que mostrar podria ser un array de objetos con esas propiedades*/
                 setBackVerifications({
                     ...backVerifications,
-                    [response.data.input]: response.data.alertMessage,
+                    [response.data.params]: response.data.alertMessage,
                 })
                 setSuccess(false)
             }
@@ -30,7 +28,18 @@ function usePost(URI) {
         }
     }
 
-    return { postForm, error, success, backVerifications }
+    // Funcion para que todas las verificaciones del back esten como nulas
+
+    const setNull = () => {
+        for (let i = 0; i < verifications.length; i++) {
+            setBackVerifications(prevBackVerifications => ({
+                ...prevBackVerifications,
+                [verifications[i].id]: null,
+            }))
+        }
+    }
+
+    return { postForm, setNull, error, success, backVerifications }
 }
 
 export default usePost;
