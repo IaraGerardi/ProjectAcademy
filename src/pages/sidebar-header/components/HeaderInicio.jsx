9e67324d2be3cdd98298611052883d45786af309
SebastianAvila00 <sebastian.avila@arbusta.net>
2022-10-20 10:56:11 -0300

    Refactory-sa: cambios en el responsive de header y orientaed

--- src/pages/sidebar-header/components/HeaderInicio.jsx
+++ src/pages/sidebar-header/components/HeaderInicio.jsx
@@ -7,19 +7,19 @@ import LogoPerfil from "../icons/logo-perfil.svg"
 import cerrarSesion from "../icons/logo-cerrar.svg"
 
 import "../style/header.css"
- import {types} from "../../../store/StoreReducer"; 
+import { types } from "../../../store/StoreReducer";
 import { useContext } from "react";
 import StoreContext from "../../../store/StoreProvider";
- 
+
 
 function HeaderInicio({ propNamePage }) {
-    const [store,dispatch]=useContext(StoreContext)
-     
+    const [store, dispatch] = useContext(StoreContext)
+
 
 
     const [infoAdmin, setInfoAdmin] = useState([]);
     const [active, setActive] = useState(false);
-  /*   const navigate = useNavigate() */
+    /*   const navigate = useNavigate() */
     const usuario = localStorage.getItem('usuario')
     const parseado = JSON.parse(usuario)
 
@@ -44,10 +44,10 @@ function HeaderInicio({ propNamePage }) {
         try {
             const resLogout = await axios.get(`${process.env.REACT_APP_BASE_URL}/logout`, { withCredentials: true });
             setInfoAdmin(resLogout.data);
-           
+
             localStorage.removeItem("usuario")
-           dispatch({type:types.authLogout})
-           /*  navigate('/login') */
+            dispatch({ type: types.authLogout })
+            /*  navigate('/login') */
         } catch (error) {
             console.log(error);
         }
@@ -59,18 +59,22 @@ function HeaderInicio({ propNamePage }) {
     return (
         <>
 
+
             <div className='boxMainHeader '>
                 <p className='p-header'>
                     {propNamePage === "Bienvenido/a" ? `${propNamePage} ${parseado.name}` : propNamePage}</p>
-                <img className="img-header cursor-pointer" src={require(`../../../img-back/admins/${parseado.avatar}`)} onClick={() => setActive(!active)} alt={parseado.name} />
 
-            </div>
+                <div>
+                    <img className="img-header cursor-pointer" src={require(`../../../img-back/admins/${parseado.avatar}`)} onClick={() => setActive(!active)} alt={parseado.name} />
 
-            <div className={`sesion ${active ? 'mostrar-sesion' : 'ocultar-sesion'}`}>
-                <Link to="/profile" className="p-2"><img className="logo-perfil" src={LogoPerfil} alt='icon-profile' /> Mi perfil</Link>
+                    <div className={`sesion ${active ? 'mostrar-sesion' : 'ocultar-sesion'}`}>
+                        <Link to="/profile" className="p-2"><img className="logo-perfil" src={LogoPerfil} alt='icon-profile' /> Mi perfil</Link>
 
-                <Link to="/"><img className="logo-perfil" src={cerrarSesion} alt='icon-logout' /><button onClick={() => { getLogout()  }}>Cerrar sesión</button></Link>
-              
+                        <Link to="/"><img className="logo-perfil" src={cerrarSesion} alt='icon-logout' /><button onClick={() => { getLogout() }}>Cerrar sesión</button></Link>
+
+                    </div>
+                </div>
+
             </div>
         </>
 
