b4a7485b63d3b4807b5703b38f7e703563daf8d9
goncataldo <gonzalo.cataldo@arbusta.net>
2022-11-02 12:46:04 -0300

    changes on news seed

--- back/database/seed.js
+++ back/database/seed.js
@@ -349,43 +349,43 @@ const news = [
   },
   {
     titulo: "Hunted, The",
-    content: "ipsum primis in faucibus orci luctus et ultrices posuere cubilia",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     link: "http://army.mil/vehicula/consequat/morbi/a/ipsum/integer.aspx?tellus=vitae&semper=consectetuer&interdum=eget&mauris=rutrum&ullamcorper=at&purus=lorem&sit=integer&amet=tincidunt&nulla=ante&quisque=vel&arcu=ipsum&libero=praesent&rutrum=blandit&ac=lacinia&lobortis=erat&vel=vestibulum&dapibus=sed&at=magna&diam=at&nam=nunc&tristique=commodo&tortor=placerat",
   },
   {
     titulo: "It's a Wonderful Afterlife",
     content:
-      "massa donec dapibus duis at velit eu est congue elementum in hac habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     link: "https://wordpress.org/tincidunt/ante/vel/ipsum/praesent/blandit/lacinia.html?aenean=ligula&lectus=suspendisse&pellentesque=ornare&eget=consequat&nunc=lectus&donec=in&quis=est&orci=risus&eget=auctor&orci=sed&vehicula=tristique&condimentum=in&curabitur=tempus&in=sit&libero=amet&ut=sem&massa=fusce&volutpat=consequat&convallis=nulla&morbi=nisl&odio=nunc&odio=nisl&elementum=duis&eu=bibendum&interdum=felis&eu=sed&tincidunt=interdum&in=venenatis&leo=turpis&maecenas=enim&pulvinar=blandit&lobortis=mi&est=in&phasellus=porttitor&sit=pede&amet=justo&erat=eu&nulla=massa&tempus=donec&vivamus=dapibus&in=duis&felis=at&eu=velit",
   },
   {
     titulo: "Dead Rising: Watchtower",
     content:
-      "vel pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in",
-    link: "http://amazon.co.uk/consequat/nulla.aspx?nullam=odio&molestie=cras&nibh=mi&in=pede&lectus=malesuada&pellentesque=in&at=imperdiet&nulla=et&suspendisse=commodo&potenti=vulputate&cras=justo&in=in&purus=blandit&eu=ultrices&magna=enim&vulputate=lorem&luctus=ipsum&cum=dolor&sociis=sit&natoque=amet&penatibus=consectetuer&et=adipiscing&magnis=elit&dis=proin&parturient=interdum&montes=mauris&nascetur=non&ridiculus=ligula&mus=pellentesque&vivamus=ultrices&vestibulum=phasellus&sagittis=id&sapien=sapien&cum=in&sociis=sapien&natoque=iaculis&penatibus=congue&et=vivamus&magnis=metus&dis=arcu&parturient=adipiscing&montes=molestie&nascetur=hendrerit&ridiculus=at&mus=vulputate&etiam=vitae&vel=nisl&augue=aenean&vestibulum=lectus&rutrum=pellentesque&rutrum=eget&neque=nunc&aenean=donec&auctor=quis&gravida=orci&sem=eget&praesent=orci&id=vehicula&massa=condimentum&id=curabitur&nisl=in&venenatis=libero&lacinia=ut&aenean=massa&sit=volutpat&amet=convallis&justo=morbi&morbi=odio&ut=odio&odio=elementum&cras=eu&mi=interdum&pede=eu&malesuada=tincidunt&in=in&imperdiet=leo&et=maecenas&commodo=pulvinar&vulputate=lobortis&justo=est",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+     link: "http://amazon.co.uk/consequat/nulla.aspx?nullam=odio&molestie=cras&nibh=mi&in=pede&lectus=malesuada&pellentesque=in&at=imperdiet&nulla=et&suspendisse=commodo&potenti=vulputate&cras=justo&in=in&purus=blandit&eu=ultrices&magna=enim&vulputate=lorem&luctus=ipsum&cum=dolor&sociis=sit&natoque=amet&penatibus=consectetuer&et=adipiscing&magnis=elit&dis=proin&parturient=interdum&montes=mauris&nascetur=non&ridiculus=ligula&mus=pellentesque&vivamus=ultrices&vestibulum=phasellus&sagittis=id&sapien=sapien&cum=in&sociis=sapien&natoque=iaculis&penatibus=congue&et=vivamus&magnis=metus&dis=arcu&parturient=adipiscing&montes=molestie&nascetur=hendrerit&ridiculus=at&mus=vulputate&etiam=vitae&vel=nisl&augue=aenean&vestibulum=lectus&rutrum=pellentesque&rutrum=eget&neque=nunc&aenean=donec&auctor=quis&gravida=orci&sem=eget&praesent=orci&id=vehicula&massa=condimentum&id=curabitur&nisl=in&venenatis=libero&lacinia=ut&aenean=massa&sit=volutpat&amet=convallis&justo=morbi&morbi=odio&ut=odio&odio=elementum&cras=eu&mi=interdum&pede=eu&malesuada=tincidunt&in=in&imperdiet=leo&et=maecenas&commodo=pulvinar&vulputate=lobortis&justo=est",
   },
   {
     titulo: "Aningaaq",
     content:
-      "mattis pulvinar nulla pede ullamcorper augue a suscipit nulla elit ac nulla sed vel enim sit amet nunc",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     link: "http://storify.com/mauris/sit/amet.aspx?ipsum=imperdiet&aliquam=sapien&non=urna&mauris=pretium&morbi=nisl&non=ut&lectus=volutpat&aliquam=sapien&sit=arcu&amet=sed&diam=augue&in=aliquam&magna=erat&bibendum=volutpat&imperdiet=in&nullam=congue&orci=etiam&pede=justo&venenatis=etiam&non=pretium&sodales=iaculis&sed=justo&tincidunt=in&eu=hac&felis=habitasse&fusce=platea&posuere=dictumst&felis=etiam&sed=faucibus&lacus=cursus&morbi=urna&sem=ut&mauris=tellus&laoreet=nulla&ut=ut&rhoncus=erat&aliquet=id&pulvinar=mauris&sed=vulputate&nisl=elementum&nunc=nullam&rhoncus=varius&dui=nulla&vel=facilisi&sem=cras&sed=non&sagittis=velit&nam=nec&congue=nisi&risus=vulputate&semper=nonummy",
   },
   {
     titulo: "Remorques (Stormy Waters)",
     content:
-      "nunc nisl duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa donec dapibus duis at velit",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
     link: "https://tumblr.com/enim.jsp?nisl=in&aenean=felis&lectus=eu&pellentesque=sapien&eget=cursus&nunc=vestibulum&donec=proin&quis=eu&orci=mi&eget=nulla&orci=ac&vehicula=enim&condimentum=in&curabitur=tempor&in=turpis&libero=nec&ut=euismod&massa=scelerisque&volutpat=quam&convallis=turpis&morbi=adipiscing&odio=lorem&odio=vitae&elementum=mattis&eu=nibh&interdum=ligula&eu=nec&tincidunt=sem&in=duis&leo=aliquam&maecenas=convallis&pulvinar=nunc&lobortis=proin&est=at&phasellus=turpis&sit=a&amet=pede&erat=posuere&nulla=nonummy&tempus=integer&vivamus=non&in=velit&felis=donec&eu=diam&sapien=neque&cursus=vestibulum&vestibulum=eget",
   },
   {
     titulo: "Tom, Tom, the Piper's Son",
     content:
-      "id consequat in consequat ut nulla sed accumsan felis ut at dolor quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae nisi nam",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     link: "https://eepurl.com/dui.png?nec=odio&nisi=in&volutpat=hac&eleifend=habitasse&donec=platea&ut=dictumst&dolor=maecenas&morbi=ut&vel=massa&lectus=quis&in=augue&quam=luctus&fringilla=tincidunt&rhoncus=nulla&mauris=mollis&enim=molestie&leo=lorem&rhoncus=quisque&sed=ut&vestibulum=erat&sit=curabitur&amet=gravida&cursus=nisi&id=at&turpis=nibh&integer=in&aliquet=hac&massa=habitasse&id=platea&lobortis=dictumst&convallis=aliquam&tortor=augue&risus=quam&dapibus=sollicitudin&augue=vitae&vel=consectetuer&accumsan=eget&tellus=rutrum&nisi=at&eu=lorem&orci=integer&mauris=tincidunt&lacinia=ante&sapien=vel&quis=ipsum&libero=praesent&nullam=blandit&sit=lacinia&amet=erat&turpis=vestibulum&elementum=sed&ligula=magna&vehicula=at&consequat=nunc&morbi=commodo&a=placerat&ipsum=praesent&integer=blandit",
   },
   {
     titulo: "Falling in Love Again",
     content:
-      "ultrices posuere cubilia curae duis faucibus accumsan odio curabitur convallis duis consequat dui",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     link: "https://dailymotion.com/nunc.html?egestas=mauris&metus=lacinia&aenean=sapien&fermentum=quis&donec=libero&ut=nullam&mauris=sit&eget=amet&massa=turpis&tempor=elementum&convallis=ligula&nulla=vehicula&neque=consequat&libero=morbi&convallis=a&eget=ipsum&eleifend=integer&luctus=a&ultricies=nibh&eu=in&nibh=quis&quisque=justo&id=maecenas&justo=rhoncus&sit=aliquam&amet=lacus&sapien=morbi&dignissim=quis&vestibulum=tortor&vestibulum=id&ante=nulla&ipsum=ultrices&primis=aliquet&in=maecenas&faucibus=leo&orci=odio&luctus=condimentum&et=id&ultrices=luctus&posuere=nec&cubilia=molestie&curae=sed&nulla=justo&dapibus=pellentesque&dolor=viverra&vel=pede&est=ac&donec=diam&odio=cras&justo=pellentesque&sollicitudin=volutpat&ut=dui&suscipit=maecenas&a=tristique&feugiat=est&et=et&eros=tempus&vestibulum=semper&ac=est&est=quam&lacinia=pharetra&nisi=magna&venenatis=ac&tristique=consequat&fusce=metus&congue=sapien&diam=ut&id=nunc&ornare=vestibulum&imperdiet=ante&sapien=ipsum&urna=primis&pretium=in&nisl=faucibus&ut=orci&volutpat=luctus",
   },
 ];
