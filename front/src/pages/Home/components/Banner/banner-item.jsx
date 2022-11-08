42f4fee83d698fab1b124a6c1863455627b2d742
Arbusta <iara.gerardi@arbusta.net>
2022-11-08 10:04:48 -0300

    changed the title of the home banner

--- front/src/pages/Home/components/Banner/banner-item.jsx
+++ front/src/pages/Home/components/Banner/banner-item.jsx
@@ -5,7 +5,7 @@ export function BannerItem({ news , index }) {
   return (
     <li className="animate__animated animate__fadeIn bannerItem" key={index.toString()}>
         <div className="contentItemBanner">
-          <h2 className="text-5xl font-semibold">{news.title}</h2>
+          <h2 className="text-3xl sm:text-4xl md:text-5xl font-semibold">{news.title}</h2>
         </div>
         <img src={news.bgImage} alt={news.title}/>
     </li>
